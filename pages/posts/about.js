c929dd4b40e0a62b38e3b623bbf16c35955c09fd
elyasn <elyasn@cloudbloq.io>
2022-01-18 16:18:29 +0430

    last changes

---
+++ pages/posts/about.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../../components/layout';
+
+function about(props) {
+    return (
+        <Layout>
+            <div>
+                <h1>This is about page.</h1>
+            </div>
+        </Layout>
+    );
+}
+
+export default about;
